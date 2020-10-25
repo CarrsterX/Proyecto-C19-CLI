0dae49ce6a28bbd8e965d859b8ed57e084fd053c
Claudio Carrasco <67278939+CarrsterX@users.noreply.github.com>
2020-10-24 22:35:48 -0300

    Update GitImport.py

--- GitImport.py
+++ GitImport.py
@@ -11,17 +11,25 @@ else:
     os.mkdir(aux+'/BaseDatos')
     os.chdir(aux+'/BaseDatos')
 
-aux2 = subprocess.call('apt list --installed | grep subversion',shell=True)
+def instalador():
+    
+    aux2 = subprocess.call('apt list --installed | grep subversion',shell=True)
 
-if aux2 == 1:
-    print('El programa svn no se encuentra instalado')
-    print('Desea instalarlo(S/N): ' ,end=''), 
-    aux3= input()
-    if aux3 == 's':
-        subprocess.call("sudo apt install subversion", shell=True)
-    if aux3 == 'n':
-        print("Terminando ejecucion")    
-        exit()
+    if aux2 == 1:
+        print('El programa svn no se encuentra instalado')
+        print('Desea instalarlo(S/N): ', end=''), 
+        aux3= input()
+        
+        if aux3 == 's':
+            subprocess.call("sudo apt install subversion", shell=True)
+        elif aux3 == 'n':
+            print("Terminando ejecucion")    
+            exit()
+        elif aux2 != 's' or aux2 != 'n':
+            print("Opcion ingresada invalida")
+            instalador()
+    
+    print("Importando Datos")
+    subprocess.call("svn export --force https://github.com/MinCiencia/Datos-COVID19/branches/master/output/producto1", shell=True)
 
-print("Importando Datos")
-subprocess.call("svn export --force https://github.com/MinCiencia/Datos-COVID19/branches/master/output/producto1", shell=True)
+instalador()
