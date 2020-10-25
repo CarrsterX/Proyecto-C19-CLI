061f2d09faf9075b27700a19108eec1db4c69c1e
Claudio Carrasco <67278939+CarrsterX@users.noreply.github.com>
2020-10-24 22:17:03 -0300

    Add files via upload

--- GitImport.py
+++ GitImport.py
@@ -1,11 +1,27 @@
 import subprocess
 import os
+
 aux = os.getcwd()
-if os.path.isdir(aux+'/Documentos/Modulo/BaseDatos'):
-    os.chdir(aux+'/Documentos/Modulo/BaseDatos');
-else:
-    os.mkdir(aux+'/Documentos/Modulo/BaseDatos')
-    os.chdir(aux+'/Documentos/Modulo/BaseDatos');
+
 print("Se encuentra en el directorio: "+aux)
-#if ()
+
+if os.path.isdir(aux+'/BaseDatos'):
+    os.chdir(aux+'/BaseDatos')
+else:
+    os.mkdir(aux+'/BaseDatos')
+    os.chdir(aux+'/BaseDatos')
+
+aux2 = subprocess.call('apt list --installed | grep subversion',shell=True)
+
+if aux2 == 1:
+    print('El programa svn no se encuentra instalado')
+    print('Desea instalarlo(S/N): ' ,end=''), 
+    aux3= input()
+    if aux3 == 's':
+        subprocess.call("sudo apt install subversion", shell=True)
+    if aux3 == 'n':
+        print("Terminando ejecucion")    
+        exit()
+
+print("Importando Datos")
 subprocess.call("svn export --force https://github.com/MinCiencia/Datos-COVID19/branches/master/output/producto1", shell=True)
