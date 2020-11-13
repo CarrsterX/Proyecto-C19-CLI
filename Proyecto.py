6f25522e1685e5bd3348753b8eb542d68eb54ee1
Xero999 <43941144+Xero999@users.noreply.github.com>
2020-11-13 20:21:57 -0300

    Add files via upload

--- Proyecto.py
+++ Proyecto.py
@@ -1,5 +1,12 @@
 from GitImport import instalador # importe de la funcion que obtiene la base de datos actualizada
+import pandas as pd
 
 
+def lector_data():
+    data = pd.read_csv('activos_vs_recuperados.csv',header=0) 
+    return data
 
-instalador()# funcion generadora de la base de datos 
+if __name__ == '__main__':
+    instalador()# funcion generadora de la base de datos 
+    data = lector_data()
+    print(data['activos'])
