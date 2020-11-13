0b67f852d4ba83bad33e23c56f1cff6d32e00b3f
Claudio Carrasco <67278939+CarrsterX@users.noreply.github.com>
2020-11-13 20:57:36 -0300

    Add files via upload

--- Proyecto.py
+++ Proyecto.py
@@ -1,5 +1,21 @@
 from GitImport import instalador # importe de la funcion que obtiene la base de datos actualizada
+import pandas as pd
+import subprocess
 
+def existe_data():
+    aux0=subprocess.call('find activos_vs_recuperados.csv',shell=True)
+    return aux0
 
+def lector_data():
+    aux0=existe_data()
+    if aux0 == 1:
+        instalador()
+        data = pd.read_csv('activos_vs_recuperados.csv',header=0) 
+        return data
+    if aux0 == 0:
+        data = pd.read_csv('activos_vs_recuperados.csv',header=0) 
+        return data
 
-instalador()# funcion generadora de la base de datos 
+if __name__ == '__main__':
+    data = lector_data()
+    print(data['activos'])
