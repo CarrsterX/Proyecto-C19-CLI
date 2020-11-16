e043b34bd877548310a7f9fde943110fb620d715
Claudio Carrasco <67278939+CarrsterX@users.noreply.github.com>
2020-11-15 21:52:30 -0300

    Add files via upload

--- Proyecto.py
+++ Proyecto.py
@@ -42,12 +42,46 @@ def porcentual(data):
     variaciones= {'porcentajes': lista_porcentajes}
     return variaciones
 
+def calculot(variaciones):
+    
+    print("A cuantos dias a futuro desea realizar el calculo: ", end="")
+    d=input()
+    d=int(d)
+    
+    data1=pd.read_csv('datosc19.csv',header=0)
+    aux0=data1[["Casos totales"]]
+    new_data = []
+    res = []
+    k=0
+    diccionario0={}
+    
+    for i in (range(len(aux0["Casos totales"]))):
+        diccionario = {}
+        aux1=aux0["Casos totales"][i]
+        
+    for j in (range(len(variaciones["porcentajes"]))):
+        aux3=variaciones["porcentajes"][j]
+        new_data.append(aux3)
+    
+    res=[]
+    
+    while k < d:
+        suma=sum(new_data)
+        prom=suma/len(new_data)
+        new_data.append(prom)
+        aux1=int(aux1+(aux1*prom))
+        res.append(aux1)
+        k=k+1 
+    d=str(d)
+    diccionario0={"Casos a "+d+" dias a futuro":res}
+
+    return diccionario0
 
 if __name__ == '__main__':
     data = lector_data()
     variaciones= porcentual(data)# formula = (actual - anterior)/anterior //// en caso de que anterior sea 0 ((anterior - actual)/actual)*-1
-    print(variaciones)
-    
+    finald=calculot(variaciones)
+    print(finald)
     #ahora resta aplicar los porcentajes de las variaciones en los datos de 'casos totales'
     
     
